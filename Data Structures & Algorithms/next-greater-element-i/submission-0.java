f19905a1533f2d765bf1acff732dba938e8eff35
sujal993 <112621052+sujal993@users.noreply.github.com>
2026-06-28 21:18:06 +0530

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = -1;
+
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] == nums1[i]) {
+
+                    for (int k = j + 1; k < nums2.length; k++) {
+                        if (nums2[k] > nums2[j]) {
+                            ans[i] = nums2[k];
+                            break;
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
